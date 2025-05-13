e123583c740e028f3bf3b507ad2c178f6f2f884c
abikesa <abikesa.sh@gmail.com>
2025-05-13 11:52:51 -0400

    no index.html invocation

--- kitabo/ensi/_build/html/python/deploy.py
+++ kitabo/ensi/_build/html/python/deploy.py
@@ -83,7 +83,7 @@ def main(commit_message, git_remote, ghp_remote):
         "pdfs", "figures", "media", "testbin", "nis", "myhtml", "dedication", "python", "ai",
         "r", "stata", "bash", "xml", "data", "aperitivo", "antipasto", "primo", "secondo",
         "contorno", "insalata", "formaggio-e-frutta", "dolce", "caffe", "digestivo", "ukubona",
-        "the-rug", "spjd-rebuild", "spjd-beta", "ukuvula", "tokens"
+        "the-rug", "spjd-rebuild", "spjd-beta", "ukuvula", "tokens", "ukuvela"
     ]
     for d in extras:
         if os.path.isdir(d):
